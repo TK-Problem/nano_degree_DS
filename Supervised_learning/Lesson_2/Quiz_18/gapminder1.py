8a0c36df91ec9bd688e4137f45a57284cc19eebe
Tomas <t.uzdavinys@gmail.com>
2018-07-30 18:22:47 +0200

    install scipy and sklearn

---
+++ Supervised_learning/Lesson_2/Quiz_18/gapminder1.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+# Assign the dataframe to this variable.
+bmi_life_data = pd.read_csv("bmi_and_life_expectancy.csv")
+
+# Make and fit the linear regression model
+#TODO: Fit the model and Assign it to bmi_life_model
+bmi_life_model = None
+
+# Make a prediction using the model
+# TODO: Predict life expectancy for a BMI value of 21.07931
+laos_life_exp = None
