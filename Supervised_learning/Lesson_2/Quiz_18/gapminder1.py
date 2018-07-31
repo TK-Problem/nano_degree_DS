6e985a922e139f718dbeeecd2462955386efe24d
Tomas <t.uzdavinys@gmail.com>
2018-07-31 18:21:08 +0200

    Completed lesson 2 quiz 20

--- Supervised_learning/Lesson_2/Quiz_18/gapminder1.py
+++ Supervised_learning/Lesson_2/Quiz_18/gapminder1.py
@@ -5,9 +5,9 @@ from sklearn.linear_model import LinearRegression
 bmi_life_data = pd.read_csv("bmi_and_life_expectancy.csv")
 
 # Make and fit the linear regression model
-#TODO: Fit the model and Assign it to bmi_life_model
-bmi_life_model = None
+bmi_life_model = LinearRegression()
+bmi_life_model.fit(bmi_life_data["BMI"].values.reshape(-1,1),
+                   bmi_life_data["Life expectancy"].values.reshape(-1,1))
 
 # Make a prediction using the model
-# TODO: Predict life expectancy for a BMI value of 21.07931
-laos_life_exp = None
+laos_life_exp = bmi_life_model.predict([ [21.07931] ])
---
+++ Supervised_learning/Lesson_2/Quiz_25/poly_reg.py
@@ -0,0 +1,22 @@
+# TODO: Add import statements
+
+# Assign the data to predictor and outcome variables
+# TODO: Load the data
+train_data = None
+X = None
+y = None
+
+# Create polynomial features
+# TODO: Create a PolynomialFeatures object, then fit and transform the
+# predictor feature
+poly_feat = None
+X_poly = None
+
+# Make and fit the polynomial regression model
+# TODO: Create a LinearRegression object and fit it to the polynomial predictor
+# features
+poly_model = None
+
+# Once you've completed all of the steps, select Test Run to see your model
+# predictions against the data, or select Submit Answer to check if the degree
+# of the polynomial features is the same as ours!
